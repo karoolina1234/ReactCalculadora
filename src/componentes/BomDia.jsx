b5dd167199387cae24adfed82ac4ff6993783ed9
Karolina De Souza Mendes <karolina-mendes@PROCERGS.REDERS>
2019-07-10 12:35:05 -0300

    Calculadora em react

---
+++ src/componentes/BomDia.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+
+export default props => <div>
+                            <h1>Bom dia {props.nome+ props.idade}!</h1>
+                            <h2>Até nunca mais</h2>
+                        </div>
